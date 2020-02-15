bfc9e38e501da11336164511dd814f628396a0a9
wxwanwan <494706193@qq.com>
2020-02-15 13:50:01 +0800

    修复初始化问题

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 这个仓库用于保存一些JS基础功能/结构的模拟实现
 > new Set()
+- [Set.prototype.size]
+- [Set.prototype.add()]
+- [Set.prototype.clear()]
+- [Set.prototype.delete()]
+- [Set.prototype.entries()]
+- [Set.prototype.forEach()]
+- [Set.prototype.has()]
+- [Set.prototype.values()]
--- set.js
+++ set.js
@@ -1,81 +1,78 @@
 class Set {
-    constructor(props) {
-        this.items = {};
-        this.size = 0;
-        this.init(props);
+  constructor(props) {
+    this.items = {};
+    this.size = 0;
+    this.init(props);
+  }
+  init(property = []) {
+    this.items = {};
+    property.forEach((element, index) => {
+      if (!Object.values(this.items).includes(element)) {
+        this.items[index] = element
+      }
+    });
+    this.size = Object.keys(this.items).length
+  }
+  has(property) {
+    return Object.values(this.items).includes(property)
+  }
+  clear() {
+    this.items = {}
+    this.size = 0
+  }
+  add(property) {
+    if (!this.has(property)) {
+      this.items[this.size] = property;
+      this.size++
     }
-    init(property) {
-        this.items = {};
-        property.forEach((element, index) => {
-            if (!Object.values(this.items).includes(element)) {
-                this.items[index] = element
-            }
-        });
-        this.size = Object.keys(this.items).length
+  }
+  delete(property) {
+    if (this.has(property)) {
+      delete this.items[Object.values(this.items).indexOf(property)]
+      this.init(Object.values(this.items))
     }
-    has(property) {
-        return Object.values(this.items).includes(property)
-    }
-    clear() {
-        this.items = {}
-        this.size = 0
-    }
-    add(property) {
-        if (!this.has(property)) {
-            this.items[this.size] = property;
-            this.size++
+  }
+  entries() {
+    let flag = 0
+    return {
+      next: () => {
+        if (flag !== this.size) {
+          flag++
+          return {
+            value: [Object.values(this.items)[flag - 1], Object.values(this.items)[flag - 1]],
+            done: flag === this.size
+          }
+        } else {
+          return {
+            value: undefined,
+            done: true
+          }
         }
+      }
     }
-    delete(property) {
-        if (this.has(property)) {
-            delete this.items[Object.values(this.items).indexOf(property)]
-            this.init(Object.values(this.items))
+  }
+  values() {
+    let flag = 0
+    return {
+      next: () => {
+        if (flag !== this.size) {
+          flag++
+          return {
+            value: Object.values(this.items)[flag - 1],
+            done: flag === this.size
+          }
+        } else {
+          return {
+            value: undefined,
+            done: true
+          }
         }
+      }
     }
-    size() {
-        return this.size
-    }
-    entries() {
-        let flag = 0
-        return {
-            next: () => {
-                if (flag !== this.size) {
-                    flag++
-                    return {
-                        value: [Object.values(this.items)[flag - 1], Object.values(this.items)[flag - 1]],
-                        done: flag === this.size
-                    }
-                } else {
-                    return {
-                        value: undefined,
-                        done: true
-                    }
-                }
-            }
-        }
-    }
-    values() {
-        let flag = 0
-        return {
-            next: () => {
-                if (flag !== this.size) {
-                    flag++
-                    return {
-                        value: Object.values(this.items)[flag - 1],
-                        done: flag === this.size
-                    }
-                } else {
-                    return {
-                        value: undefined,
-                        done: true
-                    }
-                }
-            }
-        }
-    }
-    foreach(callback) {
-        return Object.values(this.items).forEach((element, index) => {
-            callback(element, element)
-        })
-    }
+  }
+  forEach(callback) {
+    return Object.values(this.items).forEach((element, index) => {
+      callback(element, element)
+    })
+  }
 }
